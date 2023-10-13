1a14c3a2df8204187b98ceebfa701bc59fd666ed
nqt2612001 <nqt2612001@gmail.com>
2023-10-13 23:25:40 +0700

    create BE admin manage writing

--- backend/src/main/java/com/sep490/g49/shibadekiru/service/IWritingQuestionService.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/service/IWritingQuestionService.java
@@ -1,4 +1,19 @@
 package com.sep490.g49.shibadekiru.service;
 
+import com.sep490.g49.shibadekiru.entity.Lesson;
+import com.sep490.g49.shibadekiru.entity.Writing;
+import com.sep490.g49.shibadekiru.entity.WritingQuestion;
+
+import java.util.List;
+
 public interface IWritingQuestionService {
+    public List<WritingQuestion> getWritingQuestionByWriting(Writing writing);
+
+    public WritingQuestion getWritingQuestionById(Long id);
+
+    public WritingQuestion createWritingQuestion(WritingQuestion writingQuestion);
+
+    public WritingQuestion updateWritingQuestion(Long id,WritingQuestion writingQuestion);
+
+    void deleteWritingQuestion(Long id);
 }
--- backend/src/main/java/com/sep490/g49/shibadekiru/service/IWritingService.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/service/IWritingService.java
@@ -1,4 +1,18 @@
 package com.sep490.g49.shibadekiru.service;
 
+import com.sep490.g49.shibadekiru.entity.Lesson;
+import com.sep490.g49.shibadekiru.entity.Writing;
+
+import java.util.List;
+
 public interface IWritingService {
+    public List<Writing> getWritingPartByLesson(Lesson lesson);
+
+    public Writing getWritingById(Long id);
+
+    public Writing createWriting(Writing writing);
+
+    public Writing updateWriting(Long id,Writing writing);
+
+    public void deleteWriting(Long id);
 }
