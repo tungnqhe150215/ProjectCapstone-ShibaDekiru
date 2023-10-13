a66dba44b73d041ee62c8cb3874ff2f6f8acbd1c
Bùi Xuân Đoài <buixuandoai2502@gmail.com>
2023-10-14 01:38:12 +0700

    Add function manage lesson

--- backend/src/main/java/com/sep490/g49/shibadekiru/repository/LessonRepository.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/repository/LessonRepository.java
@@ -2,8 +2,14 @@ package com.sep490.g49.shibadekiru.repository;
 
 import com.sep490.g49.shibadekiru.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+
+
 }
--- backend/src/main/java/com/sep490/g49/shibadekiru/service/ILessonService.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/service/ILessonService.java
@@ -1,4 +1,23 @@
 package com.sep490.g49.shibadekiru.service;
 
+import com.sep490.g49.shibadekiru.dto.LessonDto;
+import com.sep490.g49.shibadekiru.entity.Lesson;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
 public interface ILessonService {
+
+    List<LessonDto> getAllLessons();
+
+    Lesson createLesson(Lesson lesson);
+
+    Lesson updateLesson(Long lessonId, Lesson lesson);
+
+    void deleteLesson(Long lessonId);
+
+    LessonDto getLessonById(Long lessonId);
+
+
 }
