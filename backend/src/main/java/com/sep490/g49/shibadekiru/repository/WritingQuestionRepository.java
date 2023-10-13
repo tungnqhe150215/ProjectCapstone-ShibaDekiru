90312e72416b3e2002cc16caa8a49e3e0b32f415
nqt2612001 <nqt2612001@gmail.com>
2023-10-13 18:42:41 +0700

    update backend and model mapper

--- backend/src/main/java/com/sep490/g49/shibadekiru/repository/WritingQuestionRepository.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/repository/WritingQuestionRepository.java
@@ -1,9 +1,13 @@
 package com.sep490.g49.shibadekiru.repository;
 
+import com.sep490.g49.shibadekiru.entity.Writing;
 import com.sep490.g49.shibadekiru.entity.WritingQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WritingQuestionRepository extends JpaRepository<WritingQuestion, Long> {
+    List<WritingQuestion> findAllByWriting(Writing writing);
 }
