a498d712d70b29f7009d8e4715f05c9a78c9ebaf
Hoang Duc Toan <hoangductoan29@gmail.com>
2023-10-18 00:31:28 +0700

    Update Front Update User
    Still not get ID from user

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
       {path:'user-account', component:UserListComponent},
       {path:'user-account/userAccountDetail/:id', component:UserDetailComponent},
       {path:'user-account/create-userAccount', component:CreateUserComponent},
-      // {path:'user-account/update-userAccount', component:Up},
+      {path:'user-account/update-userAccount/:id', component:UpdateUserComponent},
       {path:'user-account/update-userAccount/:id', component:UpdateUserComponent},
       {path:'post', component:ListPostComponent},
       {path:'post/post-detail/:id', component:PostDetailComponent },
