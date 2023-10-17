efed88d866dc902121e1f7c82dd951dab5077669
Hoang Duc Toan <hoangductoan29@gmail.com>
2023-10-17 17:30:56 +0700

    Update Change Status User

--- frontend/src/app/admin/manage-user/ban-user/ban-user.component.ts
+++ frontend/src/app/admin/manage-user/ban-user/ban-user.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import { UserService } from '../user.service';
+import { UserAccount } from 'src/app/core/models/user-account';
+
 
 @Component({
   selector: 'app-ban-user',
   templateUrl: './ban-user.component.html',
   styleUrls: ['./ban-user.component.css']
 })
-export class BanUserComponent {
+export class BanUserComponent implements OnInit{
+
+  constructor( public dialogRef: MatDialogRef<BanUserComponent>,
+    private userAccountService: UserService,
+    @Inject(MAT_DIALOG_DATA) public data: number,){
+
+  }
+
+  id!:number;
+  user:UserAccount = new UserAccount;
+  banAccount(id:number){
+
+  }
+
+  ngOnInit(): void {
+   
+  }
 
 }
