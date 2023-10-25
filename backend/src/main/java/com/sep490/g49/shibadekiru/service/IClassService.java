e9c8f9768c7d21b932f7a7e7c1b77e62f9f50d81
Giang Tien Khang <khanggt2311@gmail.com>
2023-10-25 22:17:26 +0700

    CRUD Back-end Class

--- backend/src/main/java/com/sep490/g49/shibadekiru/service/IClassService.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/service/IClassService.java
@@ -2,7 +2,17 @@ package com.sep490.g49.shibadekiru.service;
 
 import com.sep490.g49.shibadekiru.entity.Class;
 
+import java.util.List;
+
 public interface IClassService {
 
     Class getClassById(Long classId);
+
+    List<Class> getAllClass();
+
+    Class createClass(Class classs);
+
+    Class updateClass(Long classId, Class classUpdate);
+
+    void deleteClass(Long classId);
 }
