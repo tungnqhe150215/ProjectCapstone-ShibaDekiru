02beca4e33008e5ef580ada580064c3c9e55dfcc
Bùi Xuân Đoài <buixuandoai2502@gmail.com>
2023-10-28 14:04:36 +0700

    update code IBookService

--- backend/src/main/java/com/sep490/g49/shibadekiru/service/IBookService.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/service/IBookService.java
@@ -1,4 +1,19 @@
 package com.sep490.g49.shibadekiru.service;
 
+import com.sep490.g49.shibadekiru.entity.Book;
+
+import java.util.List;
+
 public interface IBookService {
+
+    List<Book> getAllBooks();
+
+    Book createBook(Book book);
+
+    Book updateBook(Long bookId, Book updatedBook);
+
+    void deleteBook(Long bookId);
+
+    Book getBookById(Long bookId);
+
 }
