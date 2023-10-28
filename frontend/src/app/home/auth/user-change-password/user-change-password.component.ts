b87202323ca9f31a2372fcda6aef317f774d9a32
Hoang Duc Toan <hoangductoan29@gmail.com>
2023-10-28 11:14:02 +0700

    Create Component auth

---
+++ frontend/src/app/home/auth/user-change-password/user-change-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-change-password',
+  templateUrl: './user-change-password.component.html',
+  styleUrls: ['./user-change-password.component.css']
+})
+export class UserChangePasswordComponent {
+
+}
