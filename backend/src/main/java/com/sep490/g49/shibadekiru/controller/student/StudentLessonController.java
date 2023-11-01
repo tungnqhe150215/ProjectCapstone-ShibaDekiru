4435f4dfa74d88c622c8e5935573a5a0bfe557cc
Giang Tien Khang <khanggt2311@gmail.com>
2023-11-01 13:38:38 +0700

    Back-end: Get Listening-Question By Listening Id

--- backend/src/main/java/com/sep490/g49/shibadekiru/controller/student/StudentLessonController.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/controller/student/StudentLessonController.java
@@ -3,6 +3,7 @@ package com.sep490.g49.shibadekiru.controller.student;
 import com.sep490.g49.shibadekiru.dto.*;
 import com.sep490.g49.shibadekiru.entity.Book;
 import com.sep490.g49.shibadekiru.entity.Lesson;
+import com.sep490.g49.shibadekiru.entity.Listening;
 import com.sep490.g49.shibadekiru.service.*;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -40,16 +41,22 @@ public class StudentLessonController {
 
     IKaiwaService kaiwaService;
 
+    IListeningQuestionService listeningQuestionService;
+
+    IWritingQuestionService writingQuestionService;
+
+    IReadingQuestionService readingQuestionService;
+
     @GetMapping("/{id}/lesson")
     public List<LessonDto> getAllLessonByBook(@PathVariable (name = "id") Long bookId) {
         Book bookResponse = bookService.getBookById(bookId);
-        return lessonService.getLessonPartByBook(bookResponse).stream().map(lessonDto -> modelMapper.map(lessonDto, LessonDto.class)).collect(Collectors.toList());
+        return lessonService.getLessonPartByBook(bookResponse).stream().map(lesson -> modelMapper.map(lesson, LessonDto.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/lesson/{lessonId}/reading")
     public List<ReadingDto> getAllReadingByLesson(@PathVariable (name = "lessonId") Long lessonId) {
         Lesson lessonResponse = lessonService.getLessonById(lessonId);
-        return readingService.getAllReadingByLesson(lessonResponse).stream().map(readingDto -> modelMapper.map(readingDto, ReadingDto.class)).collect(Collectors.toList());
+        return readingService.getAllReadingByLesson(lessonResponse).stream().map(reading -> modelMapper.map(reading, ReadingDto.class)).collect(Collectors.toList());
     }
 
 
@@ -57,39 +64,53 @@ public class StudentLessonController {
     @GetMapping("/lesson/{lessonId}/writing")
     public List<WritingDto> getAllWritingByLesson(@PathVariable (name = "lessonId") Long lessonId) {
         Lesson lessonResponse = lessonService.getLessonById(lessonId);
-        return writingService.getWritingPartByLesson(lessonResponse).stream().map(writingDto -> modelMapper.map(writingDto, WritingDto.class)).collect(Collectors.toList());
+        return writingService.getWritingPartByLesson(lessonResponse).stream().map(writing -> modelMapper.map(writing, WritingDto.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/lesson/{lessonId}/listening")
     public List<ListeningDto> getAllListeningByLesson(@PathVariable (name = "lessonId") Long lessonId) {
         Lesson lessonResponse = lessonService.getLessonById(lessonId);
-        return listeningService.getListeningPartByLesson(lessonResponse).stream().map(listeningDto -> modelMapper.map(listeningDto, ListeningDto.class)).collect(Collectors.toList());
+        return listeningService.getListeningPartByLesson(lessonResponse).stream().map(listening -> modelMapper.map(listening, ListeningDto.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/lesson/{lessonId}/kanji")
     public List<KanjiDto> getAllKanjiByLesson(@PathVariable (name = "lessonId") Long lessonId) {
         Lesson lessonResponse = lessonService.getLessonById(lessonId);
-        return kanjiService.getKanjiPartByLesson(lessonResponse).stream().map(kanjiDto -> modelMapper.map(kanjiDto, KanjiDto.class)).collect(Collectors.toList());
+        return kanjiService.getKanjiPartByLesson(lessonResponse).stream().map(kanji -> modelMapper.map(kanji, KanjiDto.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/lesson/{lessonId}/vocabulary")
     public List<VocabularyDto> getAllVocabularyByLesson(@PathVariable (name = "lessonId") Long lessonId) {
         Lesson lessonResponse = lessonService.getLessonById(lessonId);
-        return vocabularyService.getVocabularyPartByLesson(lessonResponse).stream().map(vocabularyDto -> modelMapper.map(vocabularyDto, VocabularyDto.class)).collect(Collectors.toList());
+        return vocabularyService.getVocabularyPartByLesson(lessonResponse).stream().map(vocabulary -> modelMapper.map(vocabulary, VocabularyDto.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/lesson/{lessonId}/grammar")
     public List<GrammarDto> getAllGrammarByLesson(@PathVariable (name = "lessonId") Long lessonId) {
         Lesson lessonResponse = lessonService.getLessonById(lessonId);
-        return grammarService.getGrammarPartByLesson(lessonResponse).stream().map(grammarDto -> modelMapper.map(grammarDto, GrammarDto.class)).collect(Collectors.toList());
+        return grammarService.getGrammarPartByLesson(lessonResponse).stream().map(grammar -> modelMapper.map(grammar, GrammarDto.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/lesson/{lessonId}/kaiwa")
     public List<KaiwaDto> getAllKaiwaByLesson(@PathVariable (name = "lessonId") Long lessonId) {
         Lesson lessonResponse = lessonService.getLessonById(lessonId);
-        return kaiwaService.getKaiwaPartByLesson(lessonResponse).stream().map(kaiwaDto -> modelMapper.map(kaiwaDto, KaiwaDto.class)).collect(Collectors.toList());
+        return kaiwaService.getKaiwaPartByLesson(lessonResponse).stream().map(kaiwa -> modelMapper.map(kaiwa, KaiwaDto.class)).collect(Collectors.toList());
     }
 
-//    @GetMapping("/lesson/listening/{listeningId}/listening-question")
+    @GetMapping("/lesson/listening/{listeningId}/listening-question")
+    public List<ListeningQuestionDto> getAllListeningQuestionByListening (@PathVariable (name = "listeningId") Long listeningId) {
+        Listening listeningResponse = listeningService.getListeningById(listeningId);
+        return listeningQuestionService.getListeningQuestionByListening(listeningResponse).stream().map(listeningQuestion -> modelMapper.map(listeningQuestion, ListeningQuestionDto.class)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/lesson/writing/{writingId}/writing-question")
+    public List<WritingQuestionDto> getAllWritingQuestionByWriting (@PathVariable (name = "writingId") Long writingId) {
+        return null;
+    }
+
+    @GetMapping("/lesson/reading/{readingId}/reading-question")
+    public List<ReadingQuestionDto> getAllReadingQuestionByReading (@PathVariable (name = "readingId") Long readingId) {
+        return null;
+    }
 
 }
