49247b16a867cebde4d3efaddd05bd67affe53bd
nqt2612001 <nqt2612001@gmail.com>
2023-11-02 04:29:57 +0700

    create back button directive

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackButtonDirective } from './directives/back-button.directive';
+
+
+
+@NgModule({
+  declarations: [
+    BackButtonDirective
+  ],
+  exports: [
+    BackButtonDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
