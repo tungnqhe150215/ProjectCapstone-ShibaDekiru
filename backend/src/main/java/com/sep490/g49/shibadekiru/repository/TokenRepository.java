17306b985a44a57e1079db69c141d729c519bec0
Bùi Xuân Đoài <buixuandoai2502@gmail.com>
2023-11-01 14:00:35 +0700

    code update repository

--- backend/src/main/java/com/sep490/g49/shibadekiru/repository/TokenRepository.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/repository/TokenRepository.java
@@ -1,10 +1,13 @@
 package com.sep490.g49.shibadekiru.repository;
 
 import com.sep490.g49.shibadekiru.entity.Token;
+import com.sep490.g49.shibadekiru.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
@@ -16,5 +19,20 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
       """)
     List<Token> findAllValidTokenByUser(Long id);
 
+    @Query(value = """
+      select t from Token t inner join UserAccount u\s
+      on t.userAccount.userAccountId = u.userAccountId\s
+      where u.userAccountId = :id and (t.expired = true or t.revoked = true)\s
+      """)
+    List<Token> findAllValidRefreshTokenByUser(Long id);
+
+    @Query("SELECT u " +
+            "FROM UserAccount u " +
+            "JOIN u.tokens t " +
+            "WHERE t.token = :token")
+    UserAccount findUserAccountByToken(String token);
+
     Optional<Token> findByToken(String token);
+
+    Optional<Token> findByUserAccountAndExpiredAndRevoked(UserAccount user, boolean expired, boolean revoked);
 }
--- backend/src/main/java/com/sep490/g49/shibadekiru/repository/UserAccountRepository.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/repository/UserAccountRepository.java
@@ -10,4 +10,8 @@ import java.util.Optional;
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     Optional<UserAccount> findByEmail(String email);
     Optional<UserAccount> findByMemberId(String memberId);
+
+    UserAccount findByUserName(String userName);
+
+    UserAccount findByResetCode(String restCode);
 }
