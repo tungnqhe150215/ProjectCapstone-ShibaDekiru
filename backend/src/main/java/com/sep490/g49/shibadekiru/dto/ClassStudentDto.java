72d3ddd9e78a22bb9b946d44f893842998d3d9c2
nqt2612001 <nqt2612001@gmail.com>
2023-11-02 04:38:35 +0700

    create class student BE

--- backend/src/main/java/com/sep490/g49/shibadekiru/dto/ClassStudentDto.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/dto/ClassStudentDto.java
@@ -14,6 +14,6 @@ public class ClassStudentDto {
 
     private Long classStudentId;
     private LocalDateTime joinedAt;
-    private StudentDto studentDto;
-    private ClassDto classDto;
+    private StudentDto student;
+    private ClassDto belongClass;
 }
