12860f26e5b1f0bc531b3cafb17d9c582abac04e
Bùi Xuân Đoài <buixuandoai2502@gmail.com>
2023-11-01 16:08:10 +0700

    update code service impl

---
+++ backend/src/main/java/com/sep490/g49/shibadekiru/dto/RegisterResponse.java
@@ -0,0 +1,24 @@
+package com.sep490.g49.shibadekiru.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sep490.g49.shibadekiru.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterResponse {
+    private String nickName;
+    private String firstName;
+    private String lastName;
+    private String memberId;
+    private String userName;
+    private String email;
+    private String password;
+    private Long roleId;
+
+}
--- backend/src/main/java/com/sep490/g49/shibadekiru/repository/UserAccountRepository.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/repository/UserAccountRepository.java
@@ -9,6 +9,9 @@ import java.util.Optional;
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     Optional<UserAccount> findByEmail(String email);
+
+    Optional<UserAccount> findByEmailOrMemberId(String email, String memberId);
+
     Optional<UserAccount> findByMemberId(String memberId);
 
     UserAccount findByUserName(String userName);
