f227642c7ef21d31cfb37d631574b7ae36027293
nqt2612001 <nqt2612001@gmail.com>
2023-11-02 11:52:32 +0700

    update function

--- frontend/src/app/core/models/class-student.ts
+++ frontend/src/app/core/models/class-student.ts
@@ -2,8 +2,8 @@ import {Class} from "./class";
 import {Student} from "./student";
 
 export class ClassStudent {
-    id?:number
-    class?:Class
-    student?:Student
-    joined_at?:Date
+    classStudentId!:number
+    class!:Class
+    student!:Student
+    joinedAt!:Date
 }
--- frontend/src/app/core/models/student.ts
+++ frontend/src/app/core/models/student.ts
@@ -1,11 +1,11 @@
 import {UserAccount} from "./user-account";
 
 export class Student {
-  id?:number
-  user?:UserAccount
-  first_name?:String
-  last_name?:String
-  email?:String
-  gender?:String
-  avatar?:String
+  id!:number
+  user!:UserAccount
+  firstName!:String
+  lastName!:String
+  email!:String
+  gender!:String
+  avatar!:String
 }
