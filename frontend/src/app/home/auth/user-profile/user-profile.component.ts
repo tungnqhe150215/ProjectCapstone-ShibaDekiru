44e882b158c79e02b1a8b100b9b891210692ac69
Hoa Cao <caohoa6296@gmail.com>
2023-11-11 00:44:27 +0700

    Update basic FE and set route for change password

--- frontend/src/app/home/auth/user-profile/user-profile.component.ts
+++ frontend/src/app/home/auth/user-profile/user-profile.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/core/services/notification.service';
+import { UseServiceService } from '../use-service.service';
+import { StorageService } from '../user-login/storage.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent {
+
+export class UserProfileComponent implements OnInit{
+
+
+  constructor(
+    private notifiService: NotificationService,
+    private userService: UseServiceService,
+    private storageService: StorageService,
+    private router: Router,
+  ){}
+
+  
+  
+  ngOnInit(): void {
+    
+  }
+
+
 
 }
