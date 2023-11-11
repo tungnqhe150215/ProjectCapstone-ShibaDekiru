8bd15955e953c12115ccf2dc30691394ccf81145
Bùi Xuân Đoài <buixuandoai2502@gmail.com>
2023-11-11 16:05:57 +0700

    update code user account controller

--- backend/src/main/java/com/sep490/g49/shibadekiru/controller/student/UserAccountController.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/controller/student/UserAccountController.java
@@ -3,9 +3,16 @@ package com.sep490.g49.shibadekiru.controller.student;
 import com.sep490.g49.shibadekiru.dto.ChangePasswordDto;
 import com.sep490.g49.shibadekiru.dto.LecturesDto;
 import com.sep490.g49.shibadekiru.dto.StudentDto;
+import com.sep490.g49.shibadekiru.dto.UserAccountDto;
+import com.sep490.g49.shibadekiru.entity.Lectures;
+import com.sep490.g49.shibadekiru.entity.Student;
+import com.sep490.g49.shibadekiru.entity.UserAccount;
 import com.sep490.g49.shibadekiru.impl.AuthenticationServiceImpl;
 import com.sep490.g49.shibadekiru.impl.UserAccountServiceImpl;
+import com.sep490.g49.shibadekiru.service.ILecturesService;
+import com.sep490.g49.shibadekiru.service.IStudentService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +26,16 @@ import java.security.Principal;
 public class UserAccountController {
 
     @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
     private UserAccountServiceImpl userAccountService;
 
+    @Autowired
+    private ILecturesService iLecturesService;
+
+    @Autowired
+    private IStudentService iStudentService;
 
 
 
@@ -32,6 +47,26 @@ public class UserAccountController {
         } catch (IllegalStateException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<StudentDto> getStudentById(@PathVariable Long id) {
+        Student student = iStudentService.getStudentByStudentId(id);
+
+        StudentDto studentDto = modelMapper.map(student, StudentDto.class);
+
+        return ResponseEntity.ok().body(studentDto);
+
+    }
+
+    @GetMapping("/lecture/{id}")
+    public ResponseEntity<LecturesDto> getLectureById(@PathVariable Long id) {
+        Lectures lectures = iLecturesService.getLectureById(id);
+
+        LecturesDto lecturesDto = modelMapper.map(lectures, LecturesDto.class);
+
+        return ResponseEntity.ok().body(lecturesDto);
+
     }
 
     @PutMapping("/student/my-profile")
--- backend/src/main/java/com/sep490/g49/shibadekiru/impl/StudentServiceImpl.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/impl/StudentServiceImpl.java
@@ -2,6 +2,7 @@ package com.sep490.g49.shibadekiru.impl;
 
 import com.sep490.g49.shibadekiru.entity.Student;
 import com.sep490.g49.shibadekiru.entity.UserAccount;
+import com.sep490.g49.shibadekiru.exception.ResourceNotFoundException;
 import com.sep490.g49.shibadekiru.repository.StudentRepository;
 import com.sep490.g49.shibadekiru.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,15 @@ public class StudentServiceImpl implements IStudentService {
     @Override
     public Student getByUserAccount(UserAccount userAccount){
         return studentRepository.findByUserAccount(userAccount);
+    }
+
+    @Override
+    public Student getStudentByStudentId(Long studentId) {
+        Student student = studentRepository.findById(studentId).orElse(null);
+        if (student == null) {
+            throw new ResourceNotFoundException("Student can not found with id: " + studentId);
+        }
+        return student;
     }
 
 }
