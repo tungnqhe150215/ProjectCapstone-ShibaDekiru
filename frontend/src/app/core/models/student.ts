8213673ff2bd650c224e08560f19366f0a828667
Hoa Cao <caohoa6296@gmail.com>
2023-11-11 18:22:32 +0700

    Update field student

--- frontend/src/app/core/models/student.ts
+++ frontend/src/app/core/models/student.ts
@@ -1,11 +1,12 @@
 import {UserAccount} from "./user-account";
 
 export class Student {
-studentId!:number
+  studentId!:number
   user!:UserAccount
   firstName!:String
   lastName!:String
   email!:String
   gender!:String
   avatar!:String
+  memberId!:String
 }
