cd4fbac65aa87de3a8947c855da7e678429b5399
Hoa Cao <hoangductoan29@gmail.com>
2023-11-14 01:17:59 +0700

    Update FE forgot password

--- frontend/src/app/home/auth/forgot-password/forgot-password.component.ts
+++ frontend/src/app/home/auth/forgot-password/forgot-password.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/core/services/notification.service';
+import { UseServiceService } from '../use-service.service';
+import { StorageService } from '../user-login/storage.service';
+import { ForgotPassword } from 'src/app/core/models/forgot-password';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit{
+
+  
+  forgotPassword!: ForgotPassword;
+  isLoggedIn = false;
+  formlogin: any = {
+    email: null,
+  };
+  constructor(
+    private notifiService: NotificationService,
+    private userService: UseServiceService,
+    private storageService: StorageService,
+    private router: Router,
+  ){}
+  ngOnInit(): void {
+    
+    
+  }
+  onSubmitEmail(){
+    this.forgotPassword = new ForgotPassword(this.formlogin.email);
+    this.userService.forgotPassword(this.forgotPassword)
+    .subscribe(
+      data =>{
+        console.log(data);
+        this.notifiService.openSnackBar('Xin vui lòng kiểm tra Email');
+      }
+    )
+  }
+
+  // handleResponse(data){
+  //   this.toa
+  // }
 
 }
