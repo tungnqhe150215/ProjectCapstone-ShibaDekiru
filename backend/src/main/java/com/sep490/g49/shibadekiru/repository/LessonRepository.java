483b20e119e2ae25188b9979337a45a0bdd52e51
Bùi Xuân Đoài <buixuandoai2502@gmail.com>
2023-11-23 16:50:37 +0700

    update code admin manager lesson controller

--- backend/src/main/java/com/sep490/g49/shibadekiru/repository/LessonRepository.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/repository/LessonRepository.java
@@ -4,13 +4,18 @@ import com.sep490.g49.shibadekiru.entity.Book;
 import com.sep490.g49.shibadekiru.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
 
     List<Lesson> findByBook(Book book);
+
+    @Query("SELECT l.book.bookId FROM Lesson l WHERE l.lessonId = :lessonId")
+    Optional<Long> findBookIdByLessonId(@Param("lessonId") Long lessonId);
 
 }
