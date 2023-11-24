4d31583ce80adcd726d5ef35b2a3d9f472173d67
Hoa Cao <caohoa6296@gmail.com>
2023-11-24 19:21:03 +0700

    Update Post list pagging

--- frontend/src/app/home/post/list-post/list-post.component.ts
+++ frontend/src/app/home/post/list-post/list-post.component.ts
@@ -12,6 +12,11 @@ export class ListPostComponent implements OnInit{
 
   id!:number;
   post: Post[] = [];
+
+  // Thêm biến cho phân trang
+  p: number = 1;
+  itemsPerPage: number = 5;
+  collectionSize: number = 0;
   constructor(
     private userPostService: UserPostService,
     private router: Router,
@@ -26,8 +31,18 @@ export class ListPostComponent implements OnInit{
     this.userPostService.getAllPost()
     .subscribe(data =>{
       this.post = data;
+      this.collectionSize = this.post.length;
       console.log(data);
     })
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.collectionSize / this.itemsPerPage);
+  }
+
+  get pages(): number[] {
+    const totalPages = this.totalPages;
+    return Array.from({ length: totalPages }, (_, index) => index + 1);
   }
 
   PostDetail(id:number){
@@ -35,5 +50,23 @@ export class ListPostComponent implements OnInit{
     this.router.navigate(['./post/post-detail',id]);
   }
 
-
+  previousPage(event: Event) {
+    event.preventDefault();
+    if (this.p > 1) {
+      this.p--;
+    }
+  }
+  
+  nextPage(event: Event) {
+    event.preventDefault();
+    if (this.p < this.totalPages) {
+      this.p++;
+    }
+  }
+  
+  goToPage(page: number, event: Event) {
+    event.preventDefault();
+    this.p = page;
+  }
+  
 }
