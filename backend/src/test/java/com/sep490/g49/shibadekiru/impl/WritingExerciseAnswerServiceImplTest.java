142b83e8377c28ebf46836086c94c0be039ff42f
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:36:55 +0700

    Create WritingExerciseAnswerServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/WritingExerciseAnswerServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WritingExerciseAnswerServiceImplTest {
+
+    @Test
+    void createWritingExerciseAnswer() {
+    }
+
+    @Test
+    void updateWritingExerciseAnswer() {
+    }
+
+    @Test
+    void getWritingExerciseAnswerByExerciseAndStudent() {
+    }
+
+    @Test
+    void getWritingExerciseAnswerByClassworkAndStudent() {
+    }
+
+    @Test
+    void checkWritingExerciseAnswerExist() {
+    }
+}
