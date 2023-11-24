2fb788986e6d2689afd56f67bfc730caeedcf749
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:20:56 +0700

    Create ClassTestAssignServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/ClassTestAssignServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassTestAssignServiceImplTest {
+
+    @Test
+    void saveClassTestAssign() {
+    }
+
+    @Test
+    void getAllClassTestRelationships() {
+    }
+
+    @Test
+    void getClassTestById() {
+    }
+
+    @Test
+    void getAllClassTestByClass() {
+    }
+
+    @Test
+    void getAllClassTestByTest() {
+    }
+
+    @Test
+    void updateExpireDate() {
+    }
+
+    @Test
+    void deleteClassTest() {
+    }
+}
