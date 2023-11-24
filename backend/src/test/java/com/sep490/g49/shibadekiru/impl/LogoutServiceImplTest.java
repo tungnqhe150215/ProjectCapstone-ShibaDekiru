3cb172471f85c14844b5c5e33ade81cccf0ff9f7
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:29:40 +0700

    Create LogoutServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/LogoutServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogoutServiceImplTest {
+
+    @Test
+    void logout() {
+    }
+}
