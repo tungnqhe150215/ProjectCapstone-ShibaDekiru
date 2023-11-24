2bc957ca45191c4c6e5e650b6f9943eef1824c01
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:26:52 +0700

    Create KatakanaServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/KatakanaServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KatakanaServiceImplTest {
+
+    @Test
+    void getAllKatakana() {
+    }
+
+    @Test
+    void getKatakanaById() {
+    }
+
+    @Test
+    void createKatakana() {
+    }
+
+    @Test
+    void updateKatakana() {
+    }
+
+    @Test
+    void deleteKatakana() {
+    }
+}
