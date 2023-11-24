ceded93ed85ac1131448dc33645b2f0cae718a16
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:31:24 +0700

    Create ReadingQuestionServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/ReadingQuestionServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReadingQuestionServiceImplTest {
+
+    @Test
+    void getReadingQuestionByReading() {
+    }
+
+    @Test
+    void getReadingQuestionById() {
+    }
+
+    @Test
+    void createReadingQuestion() {
+    }
+
+    @Test
+    void updateReadingQuestion() {
+    }
+
+    @Test
+    void deleteReadingQuestion() {
+    }
+}
