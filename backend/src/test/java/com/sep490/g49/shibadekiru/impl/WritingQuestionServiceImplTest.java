c2f21ee713ec1081b078c56c1aef03e3e5ba675c
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:37:48 +0700

    Create WritingQuestionServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/WritingQuestionServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WritingQuestionServiceImplTest {
+
+    @Test
+    void getWritingQuestionByWriting() {
+    }
+
+    @Test
+    void getWritingQuestionById() {
+    }
+
+    @Test
+    void createWritingQuestion() {
+    }
+
+    @Test
+    void updateWritingQuestion() {
+    }
+
+    @Test
+    void deleteWritingQuestion() {
+    }
+}
