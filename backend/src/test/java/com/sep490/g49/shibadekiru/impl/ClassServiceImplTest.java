ad04fff0af1162ea7d3799b38f5d24d6ada6b0d8
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:20:14 +0700

    Create ClassServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/ClassServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassServiceImplTest {
+
+    @Test
+    void getAllClassByLecture() {
+    }
+
+    @Test
+    void getClassById() {
+    }
+
+    @Test
+    void createClass() {
+    }
+
+    @Test
+    void updateClass() {
+    }
+
+    @Test
+    void deleteClass() {
+    }
+
+    @Test
+    void getAllClass() {
+    }
+
+    @Test
+    void updateIsLocked() {
+    }
+}
