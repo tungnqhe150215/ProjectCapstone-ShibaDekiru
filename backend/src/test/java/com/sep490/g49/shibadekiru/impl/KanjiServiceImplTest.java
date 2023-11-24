a945808c9edb4efd0bf68288fcc6447a1b4a9988
khanggt2311 <khanggt2311@gmail.com>
2023-11-24 16:26:15 +0700

    Create KanjiServiceImplTest.java

---
+++ backend/src/test/java/com/sep490/g49/shibadekiru/impl/KanjiServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.sep490.g49.shibadekiru.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KanjiServiceImplTest {
+
+    @Test
+    void getKanjiPartByLesson() {
+    }
+
+    @Test
+    void createKanji() {
+    }
+
+    @Test
+    void getAllKanji() {
+    }
+
+    @Test
+    void getKanjiById() {
+    }
+
+    @Test
+    void updateKanji() {
+    }
+
+    @Test
+    void deleteKanji() {
+    }
+}
