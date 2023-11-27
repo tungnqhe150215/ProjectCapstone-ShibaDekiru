7d42321994bf2a50fdb9eb135240704d1a9dee5e
nqt2612001 <nqt2612001@gmail.com>
2023-11-28 01:55:38 +0700

    Join class FE

--- frontend/src/app/home/class/student-class.service.ts
+++ frontend/src/app/home/class/student-class.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Test} from "../../core/models/test";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Class} from "../../core/models/class";
+import {ClassStudent} from "../../core/models/class-student";
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,16 @@ export class StudentClassService {
 
   getClassById(id: number): Observable<Class> {
     return this.httpClient.get<Class>(`${this.baseUrl}/class/${id}`);
+  }
+
+  joinClassByClassCode(studentId: number,code:string): Observable<Object>{
+    const param = new HttpParams().set("studentId",studentId)
+      .set("code",code);
+    return this.httpClient.post(`${this.baseUrl}/class/join`,null,{params: param})
+  }
+
+  getClassByStudent(id:number): Observable<ClassStudent[]> {
+    const params = new HttpParams().set("studentId",id)
+    return this.httpClient.get<ClassStudent[]>(`${this.baseUrl}/class/list`,{params:params});
   }
 }
