60060cd1d3c9e7cc38c2b2a4bbe62db84368f207
Hoang Duc Toan <hoangductoan29@gmail.com>
2023-12-02 02:57:24 +0700

    Update API get Lastest Post

--- backend/src/main/java/com/sep490/g49/shibadekiru/repository/PostRepository.java
+++ backend/src/main/java/com/sep490/g49/shibadekiru/repository/PostRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByLecture(Lectures lectures);
-
+    List<Post> findTop4ByOrderByCreatedAtDesc();
 }
