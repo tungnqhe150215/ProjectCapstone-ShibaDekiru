8dc418c47c5a2c83972d62f9f70993b3aa516e36
Hoa Cao <caohoa6296@gmail.com>
2023-12-03 01:08:13 +0700

    Update notifcation and vietnamese alphabet

--- frontend/src/app/admin/manage-alphabet/update-hiragana/update-hiragana.component.ts
+++ frontend/src/app/admin/manage-alphabet/update-hiragana/update-hiragana.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hiragana } from 'src/app/core/models/hiragana';
 import { HiraganaService } from '../alphabet-services/hiragana.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-update-hiragana',
@@ -14,7 +15,8 @@ export class UpdateHiraganaComponent {
   constructor(
     private hiraganaService: HiraganaService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private nofiService: NotificationService
   ) {}
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
@@ -30,6 +32,7 @@ export class UpdateHiraganaComponent {
     this.hiraganaService.updateHiragana(this.id, this.hiragana).subscribe(
       (data) => {
         this.goToHiraganasList();
+        this.nofiService.openSnackBar('Cập nhật hiragana thành công');
       },
       (error) => console.log(error)
     );
