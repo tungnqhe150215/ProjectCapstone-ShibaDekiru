af67a17d63f5008646ab5218a6ff7dcec96dda7b
Hoa Cao <caohoa6296@gmail.com>
2023-12-05 02:41:15 +0700

    Update protected Page

--- frontend/src/app/home/class/class-list/class-list.component.ts
+++ frontend/src/app/home/class/class-list/class-list.component.ts
@@ -6,6 +6,8 @@ import {StudentClassService} from "../student-class.service";
 import {SessionStorageService} from "../../../shared/services/session-storage.service";
 import {data} from "autoprefixer";
 import {ClassStudent} from "../../../core/models/class-student";
+import { StorageService } from '../../auth/user-login/storage.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-class-list',
@@ -18,11 +20,15 @@ export class ClassListComponent implements OnInit{
   joinC: Class = new Class()
   userId!: number;
   code!: string;
+  currentUser: any;
+  isLoggedIn = false;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private studentClass: StudentClassService,
-              private sessionStorage: SessionStorageService) {
+              private sessionStorage: SessionStorageService,
+              private storageService: StorageService,
+              private nofiService: NotificationService,) {
   }
 
   ngOnInit(): void {
@@ -30,19 +36,33 @@ export class ClassListComponent implements OnInit{
   }
 
   getListClass(){
+    this.currentUser = this.storageService.getUser();
+    this.isLoggedIn = this.storageService.isLoggedIn();
+    if(this.isLoggedIn){
     this.userId = this.sessionStorage.getJsonData("auth-user").userAccountId
     this.studentClass.getClassByStudent(this.userId).subscribe(data => {
       this.joinedClass = data
       console.log(data)
-    })
+    })}
+    else{
+      this.router.navigate(['/home']);
+      this.nofiService.openSnackBar('Bạn hãy đăng nhập đã');
+    }
   }
 
   joinClass(){
+    this.currentUser = this.storageService.getUser();
+    this.isLoggedIn = this.storageService.isLoggedIn();
+    if(this.isLoggedIn){
     this.studentClass.joinClassByClassCode(this.userId,this.code).subscribe(data => {
       console.log(data)
       this.joinC = data as Class
       this.goToClass(this.joinC.classId)
-    })
+    })}
+    else{
+      this.router.navigate(['/home']);
+      this.nofiService.openSnackBar('Bạn hãy đăng nhập đã');
+    }
   }
 
   goToClass(classId:number){
