47e65cf7fbe1feea9dd2e4bcb5c4c22f163d57cc
Hoang Duc Toan <hoangductoan29@gmail.com>
2023-12-10 22:02:22 +0700

    Update List Comment and Delete Comment in lecturers

--- frontend/src/app/lecturers/lecturers-post/lec-post.service.ts
+++ frontend/src/app/lecturers/lecturers-post/lec-post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from 'src/app/core/models/post';
 import { Observable } from 'rxjs';
+import { Comment } from 'src/app/core/models/comment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,8 @@ export class LecPostService {
 
   private baseURL = "http://localhost:8080/api/admin/post";
   private baseLecURL = "http://localhost:8080/api/lecture";
+  comment: Comment[] =[];
+  private baseCommentURL = "http://localhost:8080/api/post";
   post: Post[] = [];
 
   constructor(private httpClient: HttpClient) { }
@@ -41,5 +44,13 @@ export class LecPostService {
 
   updatePost(id: number, post:Post): Observable<Object>{
     return this.httpClient.put(`${this.baseLecURL}/post/${id}`,post);
+  }
+
+  getAllComment(id:number ): Observable<Comment[]>{
+    return this.httpClient.get<Comment[]>(`${this.baseCommentURL}/${id}/comment`);
+  }
+
+  deleteComment(id: number,userID:number, comtId:number):Observable<Object>{
+    return this.httpClient.delete(`${this.baseCommentURL}/${id}/comment/${userID}/${comtId}`);
   }
 }
